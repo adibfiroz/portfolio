208a6b966de290e383dbc02798a3689c7cac1188
adibfiroz <adibfiroz9702@gmail.com>
2023-05-13 16:00:13 +0530

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -18,9 +18,9 @@ const Home = () => {
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
-
       document.querySelector(this.getAttribute("href")).scrollIntoView({
         behavior: "smooth",
+        block: "center",
       });
     });
   });
