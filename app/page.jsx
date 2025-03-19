d01a1fbab4e81875c0227d06c7f40f905fa3a201
adibfiroz <adibfiroz9702@gmail.com>
2025-03-19 15:50:51 +0530

    update views

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,7 @@
 import "./home.scss";
 import HomeComponents from "../components/Home-components";
-import UpdateViews from "./actions/update-views";
 
 const Home = async () => {
-  await UpdateViews();
   return (
     <div>
       <HomeComponents />
